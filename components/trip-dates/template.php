797c9da66ec0bf07142adccf7668246c235588dc
Your Name <your_email@address>
2026-05-08 15:30:06 +0100

    Trip-dates extra fields removed so they don't render, styling updated to use btn default stylings

--- components/trip-dates/template.php
+++ components/trip-dates/template.php
@@ -8,21 +8,14 @@
                     <li class="trip-dates__item <?= $row['is_sold_out'] ? 'is-sold-out' : '' ?>">
                         <h6 class="trip-dates__item__label">
                             <?= esc_html($row['label']) ?>
-                            <?php if ($row['nights']) { ?>
-                                <span class="trip-dates__item__nights">(<?= esc_html($row['nights']) ?> nights)</span>
-                            <?php } ?>
                         </h6>
 
-                        <?php if ($row['price_display']) { ?>
-                            <span class="trip-dates__item__price"><?= esc_html($row['price_display']) ?></span>
-                        <?php } ?>
-
                         <?php if ($row['is_bookable']) { ?>
-                            <a href="<?= esc_url($row['booking_url']) ?>" class="trip-dates__item__cta button btn" target="_blank" rel="noopener">
+                            <a href="<?= esc_url($row['booking_url']) ?>" class="trip-dates__item__cta btn" target="_blank" rel="noopener">
                                 <?= esc_html__('Book Now', 'gust') ?>
                             </a>
                         <?php } elseif ($row['is_sold_out']) { ?>
-                            <button class="trip-dates__item__cta trip-dates__item__cta--sold-out button btn" type="button" disabled>
+                            <button class="trip-dates__item__cta trip-dates__item__cta--sold-out btn btn--inactive" type="button" disabled>
                                 <?= esc_html($row['sold_out_label']) ?>
                             </button>
                         <?php } ?>
