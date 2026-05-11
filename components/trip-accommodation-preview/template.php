1fd596dff7afb1997ed9a9590fd5b916b9c3a302
Your Name <your_email@address>
2026-05-11 14:15:50 +0100

    TripAccomodationPreview updated to apply correct button styling to btn--theme-2 as per design brief

--- components/trip-accommodation-preview/template.php
+++ components/trip-accommodation-preview/template.php
@@ -68,7 +68,7 @@
                     <?= \Gust\Components\Link::make(
                         title: __('View accommodation', 'gust'),
                         url: $this->url,
-                        classes: ['btn', 'btn--secondary'],
+                        classes: ['btn', 'btn--theme-2'],
                     ); ?>
                 <?php } ?>
             </div>
